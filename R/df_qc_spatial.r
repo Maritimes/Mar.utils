a25fd7079743bdec8573e8618446863adb43c4d9
mike <mike.mcmahon@dfo-mpo.gc.ca>
2018-01-11 09:51:50 -0400

    Replacing Mar.datawrangling with this one

--- R/df_qc_spatial.r
+++ R/df_qc_spatial.r
@@ -1,12 +1,15 @@
 #' @title df_qc_spatial
-#' @description This function drops points that can't be plotted from a data frame.
+#' @description This function identifies points that aren't in the northern 
+#' hemisphere (i.e. LAT between 0 and 90) and aren't in the western hemisphere 
+#' (i.e. LON between 0 and 180).  By default, it returns the "good" points, but 
+#' the "return.bad" flag allows it to return only the "bad" points. 
 #' @param df the dataframe to be cleaned
 #' @param lat.field default is \code{LATITUDE}.  The field in the dataframe holding the latitudes
 #' @param lon.field default is \code{LONGITUDE}.  The field in the dataframe holding the longitudes
 #' @param return.bad the default is \code{FALSE}.  Normally, only the plottable points are returned.
 #' If TRUE, only the rejected points are returned.
 #' @return data.frame contents depend of the return.bad parameter
-#' @family plotting
+#' @family general_use
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
 df_qc_spatial <-function(df, lat.field = "LATITUDE", lon.field = "LONGITUDE", return.bad=FALSE){
