21d3c9f03b999604b0ac4eb026aa096faad783b3
McMahon <McMahonM@dfo-mpo.ca>
2019-03-28 14:09:43 -0300

    hexes for aggregation work

---
+++ R/data_desc_hex.r
@@ -0,0 +1,8 @@
+#' This is the contents of the hex rdata file
+#'
+#' @name hex
+#' @docType data
+#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
+#' @references \url{data_blah.com}
+#' @keywords data
+NULL
