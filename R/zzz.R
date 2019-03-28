5c2618132d0d1c891ba0712a1fdcbd3d0ed6fd58
McMahon <McMahonM@dfo-mpo.ca>
2019-03-28 14:14:34 -0300

    adds update check functionality

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname) {
+  localVer = utils::packageDescription('Mar.utils')$Version
+  packageStartupMessage(paste0("Version: ", localVer))
+}
+
+.onLoad <- function(libname, pkgname){
+  options(stringsAsFactors = FALSE)
+  updateCheck(gitPkg = 'Maritimes/Mar.utils')
+}
