c32d5c7c872af247e4a87d417ee6bc0f6d4eed64
McMahon <McMahonM@dfo-mpo.ca>
2019-06-07 14:03:22 -0300

    replaces drop NA cols with drop_cols

---
+++ R/drop_cols.R
@@ -0,0 +1,60 @@
+#' @title drop_cols
+#' @description This function takes a dataframe and drops or retains fields
+#' based on the uniformity of their values.
+#' @param df default is \code{NULL}. This is the dataframe you want to handle.
+#' @param justDropNAs default is \code{TRUE}.  If this value is TRUE, the script 
+#' will only drop fields that are solely populated with NAs.  If FALSE, it
+#' will drop/retain iother fields depending on the following parameters. 
+#' @param uniformFields default is \code{'retain'}.  This determines how the script
+#' will handle uniform fields.  It can either retain them or drop them.  Valid
+#' values are \code{'retain'} or \code{'drop'} .   
+#' \describe{ 
+#' \item{\code{'drop'}} specifies that any field that has a single value for all 
+#' entries will be dropped.  This is useful for identifying the fields that can 
+#' be used to differentiate records. 
+#' \item{\code{'retain'}} specifies that only those fields which have a single 
+#' value for all entries will be retained.  This is useful for identifying the 
+#' fields with values that are shared by all records in a dataframe. 
+#' }
+#' @param keepFields default is \code{NULL}.This is a vector of column names you 
+#' definitely want to retain, regardless of the values.
+#' @return dataframe
+#' @family general_use
+#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
+#' @export
+drop_cols<-function(df = NULL, justDropNAs = TRUE, uniformFields = 'retain', keepFields = NULL){
+  if (justDropNAs) {
+    cat("Just dropping NA columns","\n")
+    return(df[!sapply(df, function(x) all(is.na(x)))])
+  }
+
+  tempdf =df
+  #NAs tend to get ignored by the uniqueness checks so we copy the df and replace NAs with -666
+  #can't replace logicals or dates with -666, so convert these field types to numeric first
+  tempdf[, sapply(tempdf, class) %in% c('Date')]<-as.numeric(unlist(tempdf[, sapply(tempdf, class) %in% c('Date')]))
+  tempdf[, sapply(tempdf, class) %in% c('logical')]<-as.numeric(tempdf[, sapply(tempdf, class) %in% c('logical')])
+  tempdf[, sapply(tempdf, class) %in% c('character', 'numeric','integer')][is.na(tempdf[, sapply(tempdf, class) %in% c('character', 'numeric','integer')])]<--666
+
+  uniformFields <- tolower(uniformFields)
+
+    intIndex <- rownames(tempdf)
+    samies = NA
+    for (i in 1:length(names(tempdf))) {
+      these = c(with(tempdf, tapply(intIndex, tempdf[names(tempdf)[i]], FUN = function(x) length(unique(x)))))
+      if (length(these) ==1) samies =c(samies, names(tempdf)[i])
+    }
+    samies = c(samies, names(tempdf[sapply(tempdf, function(x) all(is.na(x)))]))
+    samies = samies[!is.na(samies)]
+    if (uniformFields=="drop"){
+      finalFields = names(tempdf)[!(names(tempdf) %in% samies)]
+    } else if (uniformFields=="retain"){
+      finalFields = names(tempdf)[names(tempdf) %in% samies]
+    }
+
+    finalFields = c(finalFields,keepFields)
+    if (length(finalFields)==1)df = df[names(df) %in% finalFields]
+    if (length(finalFields)>1)df = df[,names(df) %in% finalFields]
+
+  return(df)
+  # return(invisible(df))
+}
