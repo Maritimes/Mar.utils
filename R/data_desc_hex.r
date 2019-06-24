de6da262bd7c5e4130d9a2c476c6cc3c64b79bd7
McMahon <McMahonM@dfo-mpo.ca>
2019-06-24 13:23:24 -0300

    removes documentation for deprecated objects

--- R/data_desc_hex.r
+++
@@ -1,8 +0,0 @@
-#' This is the contents of the hex rdata file
-#'
-#' @name hex
-#' @docType data
-#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
-#' @references \url{data_blah.com}
-#' @keywords data
-NULL
