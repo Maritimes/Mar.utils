e90e4c84e8fc436f22a27388f4c7f27a06b418d2
McMahon <McMahonM@dfo-mpo.ca>
2020-10-01 16:00:47 -0300

    changed default input var

--- R/where_now.R
+++ R/where_now.R
@@ -1,7 +1,7 @@
 #' @title where_now
 #' @description This function indicates which function has been reached, and the 
 #' parameters it is using
-#' @param inf the default is \code{as.character(sys.calls()[[sys.nframe() - 1]])}. 
+#' @param inf the default is \code{NULL}.  WOrks with \code{as.character(sys.calls()[[sys.nframe() - 1]])}. 
 #' This is a method for extracting the function information.
 #' @param lvl the default is \code{0}. This controls how many tabs are used to 
 #' display the results.  It can be useful when functions call subfunctions
@@ -9,7 +9,7 @@
 #' @family general_use
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-where_now <- function(inf=as.character(sys.calls()[[sys.nframe() - 1]]), lvl=1){
+where_now <- function(inf=NULL, lvl=1){
   inf1 <- inf[!(inf %in% c("do.call"))]
   prefix <- paste0(rep("\t",lvl), collapse="")
   cat(prefix, inf1[1],'(',paste0(inf1[2:length(inf1)], collapse = ","),')\n', sep='')
