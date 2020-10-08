d0c66e92c04e7e15c577e89e6781e486508eca25
McMahon <McMahonM@dfo-mpo.ca>
2020-10-08 07:53:03 -0300

    adds mode

---
+++ R/Mode.R
@@ -0,0 +1,14 @@
+#' @title Mode
+#' @description This function returns the mode of a vector, preserving data type.
+#' If several values share the mode, all will be returned. 
+#' @param v Vector
+#' @family util
+#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
+#' @note stolen from https://stackoverflow.com/questions/2547402/how-to-find-the-statistical-mode
+#' @export
+Mode <- function(x) {
+  ux <- unique(x)
+  tab <- tabulate(match(x, ux))
+  res <- ux[tab == max(tab)]
+  return(res)
+}
