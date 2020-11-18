174e1ee64cd64b615cff6cdd8634b16d35bb914f
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2020-11-18 13:51:17 -0400

    Enable sf objects in identify_area

--- R/identify_area.R
+++ R/identify_area.R
@@ -1,21 +1,23 @@
 #' @title identify_area
-#' @description This function takes a df with coordinate fields in decimal
-#' degrees, and overlays it a shapefile (agg.poly.shp).  It adds a column to the 
-#' df indicating which polygon within the shapefile each point falls within.
+#' @description This function takes a dataframe (with coordinate fields in decimal
+#' degrees), (optionally) a spatial object (either a shapefile, an
+#' sp::SpatialPolygonsDataframe, or an sf::polygon), and a field name from the 
+#' polygon object.  It then overlays the df with the polygon, and determines which
+#' discrete polygon contains each point from the df, and appends a new field 
+#' containing this value.  
 #' If no polygon is provided, the df will be assessed against NAFO subdivisions.
-#' @param df a dataframe to be analyzed. If left \code{NULL}, a value for 
-#' \code{db} should be provided
+#' @param df a dataframe to be analyzed. 
 #' @param lat.field the default is \code{"LATITUDE"}. the name of the field holding latitude values 
 #' (in decimal degrees)
 #' @param lon.field the default is \code{"LONGITUDE"}.  the name of the field holding longitude 
 #' values (in decimal degrees)
-#' @param agg.poly.shp default is \code{NULL}.  This is the shapefile that has 
-#' polygons that should be checked for sufficient unique values of the 
-#' sens.fields.  If NULL, NAFO zones will be used.  Otherwise, a path to any 
-#' polygon shapefile can be provided. 
+#' @param agg.poly.shp default is \code{NULL}.  This is either the path to the 
+#' *.shp file of a shapefile, an sf spatial object; or an sp spatialpolygonsdataframe.
+#'  If NULL, NAFO zones will be used.
 #' @param agg.poly.field default is \code{NULL}.  This identifies the field within 
-#' the shapefile provided to agg.poly.shp that should be used to check for 
-#' sufficient unique values of the sens.fields.
+#' \code{agg.poly.shp} that contains the values that should be appended to the 
+#' input dataframe. If NULL, "NAFO_BEST", will be used, which is the finest
+#' resolution NAFO subdivision.
 #' @return a DataFrame with the column \code{agg.poly.field} added
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
@@ -24,46 +26,39 @@ identify_area <- function(df = NULL,
                           lon.field = "LONGITUDE",
                           agg.poly.shp = NULL,
                           agg.poly.field = NULL){  
-  #retain bad coords so we can alert the user
-  df_bad = df_qc_spatial(df = df, lat.field = lat.field, lon.field = lon.field, return.bad = TRUE)
-  #get the good ones, and convert to spatial
-  df = df_qc_spatial(df = df, lat.field = lat.field, lon.field = lon.field, return.bad = FALSE)
-  df_sp = df_to_sp(df = df, lat.field = lat.field, lon.field = lon.field, the.crs = "+init=epsg:4326")
+  #flag NA coords for future id
+  df_Orig <- df
+  df[,"tmp"]<-NA
+  df[is.na(df[,lat.field])|is.na(df[,lon.field]),"tmp" ]<- "Bad coordinate"
+  df[is.na(df[,lat.field]),lat.field] <- -89.89
+  df[is.na(df[,lon.field]),lon.field] <- -89.89
+  df_sf <- sf::st_as_sf(x = df,  coords = c(lon.field, lat.field), crs = "+init=epsg:4326")
+  
   #default to determining the NAFO_BEST column of the NAFO areas if no polygon and/or field is chosen
   if (is.null(agg.poly.shp)){
-    agg.poly= Mar.data::NAFOSubunits
-    agg.poly = suppressWarnings(sp::spTransform(x=agg.poly,CRSobj = sp::CRS("+init=epsg:4326")))
+    agg.poly= Mar.data::NAFOSubunits_sf
     if (is.null(agg.poly.field)){
       agg.poly.field = 'NAFO_BEST'
     }
   }else if (is.character(agg.poly.shp)){
-    agg.poly <- rgdal::readOGR(dsn = agg.poly.shp, verbose = FALSE)
-    if (is.na(sp::proj4string(agg.poly))) {
-      cat('\nNo projection found for input shapefile - assuming geographic.')
-      sp::proj4string(agg.poly) = sp::CRS("+init=epsg:4326")
-    }
-    #convert the shape to geographic
-    agg.poly <- sp::spTransform(x=agg.poly,CRSobj = sp::CRS("+init=epsg:4326"))
+    agg.poly <- sf::st_read(dsn = agg.poly.shp)
+  }else if (class(agg.poly.shp)=="SpatialPolygons" || class(agg.poly.shp)=="SpatialPolygonsDataFrame"){
+    agg.poly = as(agg.poly.shp, "sf")
+  }else if (any(class(agg.poly.shp)=="sf")){
+    agg.poly = agg.poly.shp
   }
-  #append the appropriate value from agg.poly
-  df_sp@data = cbind(df_sp@data,sp::over( df_sp, agg.poly , fn = NULL)[agg.poly.field])
-
-  
-  #add an error message to the unprocessable fields
-  #if numeric, add -999, otherwise a more informative msg
-  if (nrow(df_bad)>0){
-    if(sapply(df_sp@data[agg.poly.field], is.numeric)[[1]]){
-      df_bad[agg.poly.field]<- -999
-    }else{
-      df_bad[agg.poly.field] <- "Bad coordinate"
-    }
-    combined = rbind(df_sp@data, df_bad)
+  if (is.na(sf::st_crs(agg.poly))) {
+    cat('\nNo projection found for input - assuming geographic.')
+    attributes(agg.poly)$crs <- sf::st_crs(4326)
   }else{
-    combined = df_sp@data
+    agg.poly <- sf::st_transform(agg.poly, 4326)
   }
   
-  #merge good an bad records together
+  agg.poly <- suppressMessages(sf::st_join(df_sf, agg.poly))
+  agg.poly[is.na(agg.poly[,agg.poly.field]),agg.poly.field] <-"Outside of Defined Areas" 
 
-  return(invisible(combined))
-  
+  agg.poly[!is.na(agg.poly$tmp),agg.poly.field]<-sf::st_drop_geometry(agg.poly[!is.na(agg.poly$tmp),"tmp"])
+  agg.poly$tmp <- agg.poly$geometry <- NULL
+  agg.poly <-  merge(agg.poly, df_Orig[,!names(df_Orig) %in% c(agg.poly.field)])
+  return(invisible(agg.poly))
 }
