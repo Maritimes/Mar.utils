a17e97ff1b909e345d4f715a0dd020edeea80701
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-02-14 14:00:07 +0000

    adds DETS_COL_LOOKUP df

---
+++ R/DETS_COL_LOOKUP.R
@@ -0,0 +1,12 @@
+#' @title DETS_COL_LOOKUP Metadata
+#' @name DETS_COL_LOOKUP
+#' @description This is a lookup table used for coverting the codes used in the "long" marfis tables
+#' into column names for the "wide" versions of the tables.
+#' @docType data
+#' @format data.frame
+#' \describe{
+#' \item{COLUMN_DEFN_ID}{polygon identifier}
+#' \item{SUM_DOC_DEFN_COL_ID}{polygon identifier}
+#' \item{PROMPT_ENG}{polygon identifier}
+#' }
+"DETS_COL_LOOKUP"
