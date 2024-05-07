d9927d3f0e8f52b26c9a4e70543add7b133cc713
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-05-07 17:09:31 +0000

    removing some files

--- inst/vmswork.R
+++
@@ -1,4 +0,0 @@
-R.utils::sourceDirectory("c:/git/Maritimes/Mar.utils/R/", modifiedOnly=F)
-test = VMS_get_recs(dateStart = "2020-01-01", dateEnd = "2020-03-31", 
-                    vrnList = c('19832', '101019','100745'), usepkg = "roracle")
-testClean = VMS_clean_recs(test)
