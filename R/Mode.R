30efdcb561f53112c14bc519a7e73f11467471b9
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2025-03-20 13:04:27 +0000

    adds na.rm to Mode and st_err

--- R/Mode.R
+++ R/Mode.R
@@ -2,11 +2,14 @@
 #' @description This function returns the mode of a vector, preserving data type.
 #' If several values share the mode, all will be returned. 
 #' @param x Vector
+#' @param na.rm The default value is \code{FALSE}. 
 #' @family util
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @note stolen from https://stackoverflow.com/questions/2547402/how-to-find-the-statistical-mode
 #' @export
 Mode <- function(x) {
+  if (na.rm) x <- na.omit(x)
+  if (length(x) == 0) return(NA)
   ux <- unique(x)
   tab <- tabulate(match(x, ux))
   res <- ux[tab == max(tab)]
--- R/st_err.r
+++ R/st_err.r
@@ -1,10 +1,13 @@
 #' @title st_err
 #' @description Calculates the standard error 
 #' @param x  The default value is \code{NULL}.  
+#' @param na.rm The default value is \code{FALSE}. 
 #' @family util
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-st_err <- function(x = NULL) {
+st_err <- function(x = NULL, na.rm = FALSE) {
+  if (na.rm) x <- na.omit(x)
+  if (length(x) == 0) return(NA)
   stats::sd(x)/sqrt(length(x))
 }
 
