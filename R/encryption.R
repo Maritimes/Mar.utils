e0e7ef94cac4d3b8b1c297023454698a9feac004
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2025-05-13 19:21:37 +0000

    fixes to encryption

--- R/encryption.R
+++ R/encryption.R
@@ -5,8 +5,8 @@
 .get_machine_id <- function() {
   user <- Sys.info()["user"]
   host <- Sys.info()["nodename"]
-  machine_id <- digest::digest(paste0(user, host), algo = "sha256")
-  return(machine_id)
+  full_hash <- digest::digest(paste0(user, host), algo = "sha256", serialize = FALSE)
+  return(substring(full_hash, 1, 32))
 }
 
 #' @title .save_encrypted
@@ -16,13 +16,16 @@
 #' @param file Path to save to
 #' @keywords internal
 .save_encrypted <- function(object, file) {
-  key <- Mar.utils:::.get_machine_id()  # Add dot here
+  key_str <- Mar.utils:::.get_machine_id()
+  raw_key <- openssl::sha256(charToRaw(key_str))
+  iv <- openssl::rand_bytes(16)
   objname <- deparse(substitute(object))
   obj_data <- list(object)
   names(obj_data) <- objname
   serialized <- serialize(obj_data, NULL)
-  encrypted <- openssl::aes_cbc_encrypt(serialized, key = charToRaw(key))
-  writeBin(encrypted, file)
+  encrypted <- openssl::aes_cbc_encrypt(serialized, key = raw_key, iv = iv)
+  combined <- c(iv, encrypted)
+  writeBin(combined, file)
 }
 
 #' @title .load_encrypted
@@ -32,10 +35,14 @@
 #' @param envir Environment where loaded objects will be assigned
 #' @keywords internal
 .load_encrypted <- function(file, envir = parent.frame()) {
-  key <- Mar.utils:::.get_machine_id()  # Add dot here
-  encrypted <- readBin(file, "raw", file.size(file))
+  key_str <- Mar.utils:::.get_machine_id()
+  raw_key <- openssl::sha256(charToRaw(key_str))
+  combined <- readBin(file, "raw", file.size(file))
+  iv <- combined[1:16]
+  encrypted <- combined[17:length(combined)]
+  
   tryCatch({
-    serialized <- openssl::aes_cbc_decrypt(encrypted, key = charToRaw(key))
+    serialized <- openssl::aes_cbc_decrypt(encrypted, key = raw_key, iv = iv)
     obj_data <- unserialize(serialized)
     for (name in names(obj_data)) {
       assign(name, obj_data[[name]], envir = envir)
