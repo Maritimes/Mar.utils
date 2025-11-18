9b7a3c4127074bb833d0622d33b7e3cfe42c9f57
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2025-11-18 18:31:08 +0000

    removes compression for huge files - was causing PRO_SPC_INFO to fail.

--- R/encryption.R
+++ R/encryption.R
@@ -20,7 +20,6 @@
 #' @param object R object to save
 #' @param list Character vector of object names to save (NULL if using 'object')
 #' @param file Path to save to
-#' @param compress logical or character string specifying the compression method.
 #' @param envir Environment to look in for objects (when using list)
 #' @param encrypt Logical. If TRUE (default), performs encryption. If FALSE, saves normally via base::save.
 #' @family file_management
@@ -55,11 +54,32 @@ save_encrypted <- function(object = NULL,
   iv <- openssl::rand_bytes(16)
   
   serialized <- serialize(obj_data, NULL)
-  encrypted <- openssl::aes_cbc_encrypt(serialized, key = raw_key, iv = iv)
-  encrypted_base64 <- base64enc::base64encode(encrypted)
   
-  writeLines(c(base64enc::base64encode(iv), encrypted_base64), con = file)
+  chunk_size <- 50 * 1024 * 1024
   
+  if (length(serialized) > chunk_size) {
+    num_chunks <- ceiling(length(serialized) / chunk_size)
+    encrypted_chunks <- vector("list", num_chunks)
+    
+    for (i in seq_len(num_chunks)) {
+      start_idx <- (i - 1) * chunk_size + 1
+      end_idx <- min(i * chunk_size, length(serialized))
+      chunk <- serialized[start_idx:end_idx]
+      encrypted_chunks[[i]] <- openssl::aes_cbc_encrypt(chunk, key = raw_key, iv = iv)
+    }
+    
+    output_lines <- c(
+      base64enc::base64encode(iv),
+      paste0("CHUNKED:", num_chunks),
+      sapply(encrypted_chunks, base64enc::base64encode)
+    )
+  } else {
+    encrypted <- openssl::aes_cbc_encrypt(serialized, key = raw_key, iv = iv)
+    encrypted_base64 <- base64enc::base64encode(encrypted)
+    output_lines <- c(base64enc::base64encode(iv), encrypted_base64)
+  }
+  
+  writeLines(output_lines, con = file)
   invisible(NULL)
 }
 
@@ -95,20 +115,38 @@ load_encrypted <- function(file, extract_user = NULL, extract_computer = NULL, e
       raw_key <- openssl::sha256(charToRaw(key_str))
       
       lines <- readLines(con = file)
-      encrypted_base64 <- lines[2]
-      encrypted <- base64enc::base64decode(encrypted_base64)
       
-      decrypted <- tryCatch(
-        openssl::aes_cbc_decrypt(encrypted, key = raw_key, iv = first_line_raw),
-        error = function(e) {
-          stop("Unable to decrypt ", file, ".  If you did not extract this data yourself, please ensure that you are supplying the values of 'extract_user' and 'extract_computer' of the person who did.", call. = FALSE)
+      is_chunked <- grepl("^CHUNKED:", lines[2])
+      
+      if (is_chunked) {
+        num_chunks <- as.integer(sub("^CHUNKED:", "", lines[2]))
+        encrypted_chunks <- lines[3:(2 + num_chunks)]
+        
+        decrypted_chunks <- vector("list", num_chunks)
+        for (i in seq_len(num_chunks)) {
+          encrypted <- base64enc::base64decode(encrypted_chunks[i])
+          decrypted_chunks[[i]] <- openssl::aes_cbc_decrypt(encrypted, key = raw_key, iv = first_line_raw)
         }
-      )
+        
+        decrypted <- do.call(c, decrypted_chunks)
+      } else {
+        encrypted_base64 <- lines[2]
+        encrypted <- base64enc::base64decode(encrypted_base64)
+        
+        decrypted <- tryCatch(
+          openssl::aes_cbc_decrypt(encrypted, key = raw_key, iv = first_line_raw),
+          error = function(e) {
+            stop("Unable to decrypt ", file, ".  If you did not extract this data yourself, please ensure that you are supplying the values of 'extract_user' and 'extract_computer' of the person who did.", call. = FALSE)
+          }
+        )
+      }
       
-      is_compressed <- length(decrypted) >= 2 && decrypted[1] == 0x1f && decrypted[2] == 0x8b
+      is_compressed <- length(decrypted) >= 2 && 
+        ((decrypted[1] == 0x1f && decrypted[2] == 0x8b) ||
+           (decrypted[1] == 0x78))
       
       if (is_compressed) {
-        decrypted <- memDecompress(decrypted, type = "gzip")
+        decrypted <- memDecompress(decrypted, type = "unknown")
       }
       
       obj_data <- unserialize(decrypted)
