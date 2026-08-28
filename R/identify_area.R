ade0f21075f766ebc89ee9474b38c1b34d88b2ff
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2026-08-28 16:07:17 +0000

    better fixes the identify_area function to handle both shp and sf objects

--- R/identify_area.R
+++ R/identify_area.R
@@ -41,9 +41,13 @@ identify_area <- function (df = NULL,
     coord_df <-as.data.frame(st_coordinates(df))
     lat_vals <- coord_df$Y  # Now works
     lon_vals <- coord_df$X  # Now works
+    df_sf <- df
   }else{
+    df <-df  |>  
+      mutate(lat_orig = .data[[lat.field]], lon_orig = .data[[lon.field]])
     lat_vals <- df[, lat.field]
     lon_vals <- df[, lon.field]
+    df_sf <- st_as_sf(df, coords = c(lon.field, lat.field), crs = "EPSG:4326")
   }
   
   df$tmp <- NA
@@ -54,12 +58,14 @@ identify_area <- function (df = NULL,
     handled=rbind.data.frame(handled, coordMissing)
     df <-df[!is.na(lat_vals) & !is.na(lon_vals), ]
   }
+  
   if (nrow(df[(lat_vals > 90 | lat_vals < -90) | (lon_vals > 180 | lon_vals < -180), ]) > 0) {
     coordImpossible <- df[(lat_vals > 90 | lat_vals < -90) | (lon_vals > 180 | lon_vals < -180), ]
     coordImpossible$tmp <- "<impossible coord>"
     handled=rbind.data.frame(handled, coordImpossible)
     df <-df[!((lat_vals > 90 | lat_vals < -90) | (lon_vals > 180 | lon_vals < -180)), ]
   }
+  
   if (is.null(agg.poly.shp)){
     if (flag.land) {
       agg.poly = Mar.data::NAFOSubunitsLnd_sf
@@ -76,38 +82,43 @@ identify_area <- function (df = NULL,
   } else if ("sf" %in% class(agg.poly.shp)) {
     agg.poly = agg.poly.shp
   }
+  
   if (agg.poly.field %in% names(df)) {
     names(agg.poly)[names(agg.poly) == agg.poly.field] <- paste0(agg.poly.field, "_1")
     agg.poly.field <- paste0(agg.poly.field, "_1")
     warning("The value for agg.poly.field already exists in df.  The output will append '_1' to the field coming from agg_poly_shp")
   }
+  
   if (is.na(sf::st_crs(agg.poly))) {
     message("No projection found for input - assuming geographic.")
     attributes(agg.poly)$crs <- sf::st_crs(4326)
   } else {
     agg.poly <- sf::st_transform(agg.poly, 4326)
   }
-  df <-df  |>  
-    mutate(lat_orig = .data[[lat.field]], 
-           lon_orig = .data[[lon.field]])
   
   df_sf <- sf::st_as_sf(x = df, coords = c(lon.field, lat.field), crs = "EPSG:4326")
   sink <- utils::capture.output(sf::sf_use_s2(FALSE))
   res <- suppressMessages(sf::st_join(df_sf, agg.poly))
   sink <- utils::capture.output(sf::sf_use_s2(TRUE))
+
   res[which(is.na(res[, agg.poly.field])), agg.poly.field] <- "<outside known areas>"
   res[!is.na(res$tmp), agg.poly.field] <- sf::st_drop_geometry(res[!is.na(res$tmp), agg.poly.field])
   res$tmp <- res$geometry <- NULL
   res <- res[, c(names(res[names(res) %in% names(df_Orig)]), agg.poly.field)]
   res <- res[, !names(res) %in% badNames]
   res <- merge(df_Orig, res, by = "ID_")
-  bbox <- st_bbox(agg.poly)
-  res[which(res$lat_orig > bbox[4] | res$lat_orig < bbox[2] | 
-              res$lon_orig > bbox[3] | res$lon_orig < bbox[1]), 
-      agg.poly.field] <- "<outside known areas>"
-  # bbox <- as.vector(sf::st_bbox(agg.poly))
-  # res[which(res[, lat.field] > bbox[4] | res[, lat.field] < bbox[2] | res[, lon.field] > bbox[3] | res[, lon.field] < bbox[1]), agg.poly.field] <- "<outside known areas>"
-  if (nrow(handled) > 0) {
+  if(inherits(df, "sf")) {
+    bbox <- st_bbox(agg.poly)
+    res_coords <- as.data.frame(st_coordinates(res))
+    res[which(res_coords$Y > bbox[4] | res_coords$Y < bbox[2] | res_coords$X > bbox[3] | res_coords$X < bbox[1]),
+        agg.poly.field] <- "<outside known areas>"
+  } else {
+    bbox <- st_bbox(agg.poly)
+    res[which(res$lat_orig > bbox[4] | res$lat_orig < bbox[2] | res$lon_orig > bbox[3] | res$lon_orig < bbox[1]),
+        agg.poly.field] <- "<outside known areas>"
+  }
+
+    if (nrow(handled) > 0) {
     colnames(handled)[colnames(handled) == "tmp"] <- agg.poly.field
     res <- rbind.data.frame(res, handled)
   }
